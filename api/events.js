551bc449db6dcbabc34b7abf74a00c76a32c2b2d
Olamixx <ifeolayeni97@gmail.com>
2025-09-24 13:43:31 +0100

    deleted events.js

--- api/events.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Allow', ['GET']);
-  return res.status(410).json({ error: 'SSE disabled on Hobby plan. Use admin-dashboard actions for updates.' });
-}
-
-
