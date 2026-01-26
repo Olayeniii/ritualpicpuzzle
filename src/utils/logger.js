bce3e17cff99b9489d445b2b93e4c0ae9680bc0d
Olamixx <ifeolayeni97@gmail.com>
2026-01-26 14:04:16 +0100

    Utils

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -29,61 +29,60 @@ export const logger = {
 };
 
 // Filter out noisy extension console logs
+// Works in BOTH development and production
 export const suppressExtensionLogs = () => {
-  if (!isDevelopment) {
-    const originalLog = console.log;
-    const originalWarn = console.warn;
-    const originalInfo = console.info;
-    
-    // List of patterns to suppress
-    const suppressPatterns = [
-      /UserData loaded/,
-      /Loading extension data/,
-      /Loading signatures DB/,
-      /Loading settings/,
-      /received injected\.js_completed/,
-      /Signatures DB loaded/,
-      /Storage data loaded/,
-      /Settings loaded successfully/,
-      /Extension data loaded/,
-      /Access token found/,
-      /Adding.*Wallet to window/,
-      /Waiting for injector/,
-      /Injector and settings ready/,
-      /sending activateProtection/,
-      /activateProtection signal sent/,
-      /Backpack couldn't override/,
-      /ethereum/i,
-      /wallet/i,
-      /inpage\.js/,
-      /injected\.js/,
-      /evmAsk\.js/,
-      /seedProtector\.js/,
-      /addToWindow\.js/
-    ];
-    
-    const shouldSuppress = (args) => {
-      const message = args.join(' ');
-      return suppressPatterns.some(pattern => pattern.test(message));
-    };
-    
-    console.log = function(...args) {
-      if (!shouldSuppress(args)) {
-        originalLog.apply(console, args);
-      }
-    };
-    
-    console.warn = function(...args) {
-      if (!shouldSuppress(args)) {
-        originalWarn.apply(console, args);
-      }
-    };
-    
-    console.info = function(...args) {
-      if (!shouldSuppress(args)) {
-        originalInfo.apply(console, args);
-      }
-    };
-  }
+  const originalLog = console.log;
+  const originalWarn = console.warn;
+  const originalInfo = console.info;
+
+  // List of patterns to suppress
+  const suppressPatterns = [
+    /UserData loaded/,
+    /Loading extension data/,
+    /Loading signatures DB/,
+    /Loading settings/,
+    /received injected\.js_completed/,
+    /Signatures DB loaded/,
+    /Storage data loaded/,
+    /Settings loaded successfully/,
+    /Extension data loaded/,
+    /Access token found/,
+    /Adding.*Wallet to window/,
+    /Waiting for injector/,
+    /Injector and settings ready/,
+    /sending activateProtection/,
+    /activateProtection signal sent/,
+    /Backpack couldn't override/,
+    /ethereum/i,
+    /wallet/i,
+    /inpage\.js/,
+    /injected\.js/,
+    /evmAsk\.js/,
+    /seedProtector\.js/,
+    /addToWindow\.js/
+  ];
+
+  const shouldSuppress = (args) => {
+    const message = args.join(' ');
+    return suppressPatterns.some(pattern => pattern.test(message));
+  };
+
+  console.log = function(...args) {
+    if (!shouldSuppress(args)) {
+      originalLog.apply(console, args);
+    }
+  };
+
+  console.warn = function(...args) {
+    if (!shouldSuppress(args)) {
+      originalWarn.apply(console, args);
+    }
+  };
+
+  console.info = function(...args) {
+    if (!shouldSuppress(args)) {
+      originalInfo.apply(console, args);
+    }
+  };
 };
 
