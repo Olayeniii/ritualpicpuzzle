8258eb09ae2ff792e92c04c6492d53efd5541a45
Olamixx <ifeolayeni97@gmail.com>
2026-01-27 22:24:21 +0100

    removed unused var

--- src/App.js
+++ src/App.js
@@ -491,15 +491,6 @@ useEffect(() => {
     startGame();
   };
 
-  const shuffle = (array) => {
-    let arr = [...array];
-    for (let i = arr.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr;
-  };
-
   const tryMove = (i) => {
     if (gameOver) return;
 
